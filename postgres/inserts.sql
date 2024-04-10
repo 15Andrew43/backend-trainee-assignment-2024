fe184282269c1c2da76f267363c5e46ea652d069
Андрей Боровец <avborovets@MacBook-Pro-Andrej.local>
2024-04-10 11:43:56 +0300

    add table createion and some inserts

---
+++ postgres/inserts.sql
@@ -0,0 +1,10 @@
+INSERT INTO features (name) VALUES ('football'), ('basketball'), ('tennis');
+
+
+INSERT INTO tags (name) VALUES ('red'), ('blue'), ('green');
+
+
+INSERT INTO banners (feature_id, data_id, is_active) VALUES 
+(1, 'Banner Data 1', true),
+(2, 'Banner Data 2', true),
+(3, 'Banner Data 3', true);
