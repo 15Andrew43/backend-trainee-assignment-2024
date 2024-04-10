727070860983df316bb8184c8864d3b19808147f
Андрей Боровец <avborovets@MacBook-Pro-Andrej.local>
2024-04-10 13:54:53 +0300

    add validation in mongo data

--- db/mongo/create_banner_data_collection.js
+++ db/mongo/create_banner_data_collection.js
@@ -1,3 +1,20 @@
 db = db.getSiblingDB("mydatabase");
 
-db.createCollection("banner_data");
+db.createCollection("banner_data", {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            required: ["id", "content"],
+            properties: {
+                id: {
+                    bsonType: "int",
+                    description: "must be an integer and is required"
+                },
+                content: {
+                    bsonType: "string",
+                    description: "must be a string and is required"
+                }
+            }
+        }
+    }
+});
